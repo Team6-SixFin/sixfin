d0feb05ad02daf4ca64feb5f464d0e22743d75e1
choipg <choipg2684@naver.com>
2026-08-31 16:14:29 +0900

    feat : 회원가입, 로그인 구현

--- user-service/src/main/java/com/sparta/user/application/facade/UserFacade.java
+++ user-service/src/main/java/com/sparta/user/application/facade/UserFacade.java
@@ -1,10 +1,27 @@
 package com.sparta.user.application.facade;
 
+import com.sparta.user.application.dto.request.LoginRequestDto;
+import com.sparta.user.application.dto.request.SignupRequestDto;
+import com.sparta.user.application.dto.response.TokenResponseDto;
+import com.sparta.user.application.service.UserAuthService;
+import com.sparta.user.application.service.UserQueryService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
-//다른 서버스와 조합해야 하는 경우 사용
+/**
+ * Presentation - Application 중계 Facade
+ */
 @Component
 @RequiredArgsConstructor
 public class UserFacade {
+    private final UserAuthService userAuthService;
+    private final UserQueryService userQueryService;
+
+    public void signup(SignupRequestDto requestDto) {
+        userAuthService.signup(requestDto);
+    }
+
+    public TokenResponseDto login(LoginRequestDto requestDto) {
+        return userAuthService.login(requestDto);
+    }
 }
--- user-service/src/main/java/com/sparta/user/infrastructure/persistence/repository/UserRepository.java
+++ user-service/src/main/java/com/sparta/user/infrastructure/persistence/repository/UserRepository.java
@@ -3,8 +3,12 @@ package com.sparta.user.infrastructure.persistence.repository;
 import com.sparta.user.domain.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface UserRepository extends JpaRepository<User, UUID> {
-
+    boolean existsByEmailAndDeletedAtIsNull(String email);
+    boolean existsByNicknameAndDeletedAtIsNull(String nickname);
+    Optional<User> findByEmailAndDeletedAtIsNull(String email);
+    Optional<User> findByUserIdAndDeletedAtIsNull(UUID userId);
 }
