db73a7ea983a08c269caa9688f6e431c76e6e576
choipg <choipg2684@naver.com>
2026-08-31 15:12:21 +0900

    feat : UserAuthController 생성

---
+++ user-service/src/main/java/com/sparta/user/presentation/controller/UserAuthController.java
@@ -0,0 +1,8 @@
+package com.sparta.user.presentation.controller;
+
+/**
+ * 인증 관련 REST Controller (/api/auth)
+ * - 성공 응답 Wrapping 제거 및 HTTP Status + 순수 데이터 반환
+ */
+public class UserAuthController {
+}
