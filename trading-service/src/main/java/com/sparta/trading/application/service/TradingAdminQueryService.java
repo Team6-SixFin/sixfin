90940cdf135d58bdd88e314dabf1d934cefbaf3b
civalwar114 <sly114@naver.com>
2026-09-02 14:09:05 +0900

    심벌 가져오게 추가

--- trading-service/src/main/java/com/sparta/trading/application/service/TradingAdminQueryService.java
+++ trading-service/src/main/java/com/sparta/trading/application/service/TradingAdminQueryService.java
@@ -5,9 +5,11 @@ import com.sparta.trading.application.dto.query.TradingSearchAccountsQuery;
 import com.sparta.trading.application.dto.result.TradingAdminOrderQueryResult;
 import com.sparta.trading.domain.entity.Accounts;
 import com.sparta.trading.domain.entity.Orders;
+import com.sparta.trading.domain.entity.Stocks;
 import com.sparta.trading.domain.repository.accounts.TradingAccountsQueryRepository;
 import com.sparta.trading.domain.repository.order.TradingOrderQueryRepository;
 import com.sparta.trading.global.response.PageResponse;
+import com.sparta.trading.infrastructure.persistence.repository.stocks.StocksRepository;
 import com.sparta.trading.presentation.dto.response.TradigAdminOrderResponseDto;
 import com.sparta.trading.presentation.dto.response.TradingAccountsResponseDto;
 import lombok.RequiredArgsConstructor;
@@ -31,6 +33,7 @@ public class TradingAdminQueryService {
 
     private final TradingAccountsQueryRepository tradingAccountsQueryRepository;
     private final TradingOrderQueryRepository tradingOrderQueryRepository;
+    private final StocksRepository stocksRepository;
 
     public Page<TradingAccountsResponseDto> search(TradingSearchAccountsQuery tradingSearchAccountsQuery) {
         int page = tradingSearchAccountsQuery.page();
@@ -62,8 +65,14 @@ public class TradingAdminQueryService {
                 Sort.by(Sort.Direction.DESC, sort)
         );
         Page<Orders> orders = tradingOrderQueryRepository.searchOrder(pageable);
-
         List<Orders> orderList = orders.getContent();
+
+        //유저 아이디와 심벌 가져오기
+        List<Long> stockIds = orderList.stream()
+                .map(Orders::getStockId)
+                .filter(Objects::nonNull)
+                .distinct()
+                .toList();
 
         List<UUID> accountIds = orderList.stream()
                 .map(Orders::getAccountId)
@@ -71,10 +80,13 @@ public class TradingAdminQueryService {
                 .distinct()
                 .toList();
 
+        Map<Long, String> stockSymbolMap = stocksRepository.findAllById(stockIds).stream()
+                .collect(Collectors.toMap(Stocks::getId,Stocks::getSymbol));
 
         Map<UUID, UUID> accountUserMap = tradingAccountsQueryRepository.findAllById(accountIds).stream()
                 .collect(Collectors.toMap(Accounts::getId, Accounts::getUserId));
 
+        //summary 계산
         long filledCount = orderList.stream().filter(o -> "FILLED".equals(o.getStatus())).count();
         long rejectedCount = orderList.stream().filter(o -> "REJECTED".equals(o.getStatus())).count();
 
@@ -84,7 +96,7 @@ public class TradingAdminQueryService {
         );
 
         Page<TradigAdminOrderResponseDto> responsePage = orders.map(order -> {
-            String symbol = String.valueOf(order.getStockId()); // Stock 테이블 조회로 대체 권장
+            String symbol = stockSymbolMap.get(order.getStockId());
             UUID userId = accountUserMap.get(order.getAccountId());
 
             return new TradigAdminOrderResponseDto(
