11ea89ebb5328a071068ade627c6fd3ce502c9b6
choipg <choipg2684@naver.com>
2026-09-03 22:03:10 +0900

    feat : learning - 에러코드 추가

--- learning-service/src/main/java/com/sparta/learning/global/exception/LearningErrorCode.java
+++ learning-service/src/main/java/com/sparta/learning/global/exception/LearningErrorCode.java
@@ -14,6 +14,9 @@ public enum LearningErrorCode implements ErrorCode {
     FEEDBACK_NOT_FOUND(HttpStatus.NOT_FOUND, "피드백을 찾을 수 없습니다."),
     POSITION_NOT_FOUND(HttpStatus.NOT_FOUND, "포지션을 찾을 수 없습니다."),
 
+    POSITION_FIRST_TRADE_NOT_FOUND (HttpStatus.NOT_FOUND, "해당 포지션의 최초 체결 내역을 찾을 수 없습니다."),
+    POSITION_LATEST_TRADE_NOT_FOUND (HttpStatus.NOT_FOUND, "해당 포지션의 최신 체결 내역을 찾을 수 없습니다."),
+
     AI_RESPONSE_GENERATION_FAILED(HttpStatus.NOT_FOUND, "AI 피드백 생성을 실패했습니다.");
 
 
