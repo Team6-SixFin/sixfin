27df89b03e86de1cd6fe46a75c20f7d84f43380a
choipg <choipg2684@naver.com>
2026-09-07 14:56:23 +0900

    refactor : learning - application.yml AI호출시 반복호출, 호출지연 등 방지 설정 추가 및 AiClientConfig 파일 추가

---
+++ learning-service/src/main/java/com/sparta/learning/infrastructure/config/AiClientConfig.java
@@ -0,0 +1,20 @@
+package com.sparta.learning.infrastructure.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
+import org.springframework.web.client.RestClient;
+
+@Configuration
+public class AiClientConfig {
+
+    @Bean
+    public RestClient.Builder restClientBuilder() {
+        // 스프링 부트 버전에 구애받지 않는 표준 팩토리 사용
+        SimpleClientHttpRequestFactory factory = new SimpleClientHttpRequestFactory();
+        factory.setConnectTimeout(5000); // 연결 타임아웃 (5초)
+        factory.setReadTimeout(30);       // 응답 대기 타임아웃
+
+        return RestClient.builder().requestFactory(factory);
+    }
+}
