cb6acb1253a5f67d36c8a44113e01c938545a191
choipg <choipg2684@naver.com>
2026-09-08 14:39:47 +0900

    refactor : learning - 응답 대기타임 수정

--- learning-service/src/main/java/com/sparta/learning/infrastructure/config/AiClientConfig.java
+++ learning-service/src/main/java/com/sparta/learning/infrastructure/config/AiClientConfig.java
@@ -13,7 +13,7 @@ public class AiClientConfig {
         // 스프링 부트 버전에 구애받지 않는 표준 팩토리 사용
         SimpleClientHttpRequestFactory factory = new SimpleClientHttpRequestFactory();
         factory.setConnectTimeout(5000); // 연결 타임아웃 (5초)
-        factory.setReadTimeout(30);       // 응답 대기 타임아웃
+        factory.setReadTimeout(10000);       // 응답 대기 타임아웃
 
         return RestClient.builder().requestFactory(factory);
     }
