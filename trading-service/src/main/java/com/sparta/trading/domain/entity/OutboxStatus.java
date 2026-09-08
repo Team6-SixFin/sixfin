a838aee1b514288e1b322dfab4c6f1164eb31a75
joonseo21 <joonseo2012@g.skku.edu>
2026-09-08 14:14:38 +0900

    feat: Outbox 이벤트 상태 전이 규칙과 조회 쿼리 추가

--- trading-service/src/main/java/com/sparta/trading/domain/entity/OutboxStatus.java
+++ trading-service/src/main/java/com/sparta/trading/domain/entity/OutboxStatus.java
@@ -1,7 +1,18 @@
 package com.sparta.trading.domain.entity;
 
+import lombok.AllArgsConstructor;
+
+import java.util.Set;
+
+@AllArgsConstructor
 public enum OutboxStatus {
-    PENDING,
-    PUBLISHED,
-    FAILED
+    PUBLISHED(Set.of()),
+    FAILED(Set.of(PUBLISHED)),
+    PENDING(Set.of(PUBLISHED, FAILED)),
+    ;
+
+    private final Set<OutboxStatus> next;
+    public boolean validateNext(OutboxStatus next){
+        return this.next.contains(next);
+    }
 }
