858bf0b16771b47390b624b31f01c1d2eea95271
kuyhxl <ozzing727@naver.com>
2026-09-19 19:55:28 +0900

    feat: 충돌 해결 중 누락된 테스트 메서드 닫기 중괄호 복구

--- learning-service/src/test/java/com/sparta/learning/application/service/LearningCommandServiceTest.java
+++ learning-service/src/test/java/com/sparta/learning/application/service/LearningCommandServiceTest.java
@@ -448,6 +448,9 @@ class LearningCommandServiceTest {
                 ArgumentCaptor.forClass(LearningCommandService.GenerationContext.class);
         verify(aiFeedbackProcessor).processAiFeedbackAsync(contextCaptor.capture());
         assertTrue(contextCaptor.getValue().isAlreadyProcessed());
+    }
+
+    @Test
     @DisplayName("요청형: 이미 완료된 피드백은 거부되어도 COMPLETED를 유지한다")
     void keepsCompletedFeedbackWhenRejected() {
         // 체결에 변화가 없으면 feedbackKey가 같아 기존 완료 피드백을 그대로 찾는다.
